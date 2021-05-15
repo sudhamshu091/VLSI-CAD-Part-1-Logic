4a11adb24de1b6d9288fc0a264be9662ec690bb7
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-05-15 10:24:38 +0530

    Create pcn.py

---
+++ Week1/Programming Assignment/pcn.py
@@ -0,0 +1,35 @@
+from itertools import chain
+from itertools import islice
+
+def parse(filePath):
+    with open(filePath, "rb") as f:
+        try:
+            lines = iter(f)
+            numVars = int(next(lines))
+            cubeCount = int(next(lines))
+            cubes = [None]*cubeCount
+            
+            for i in range(cubeCount):
+                line = next(lines)
+                cubes[i] = tuple(islice(map(int, line.split()), 1, None))
+
+            return (numVars, tuple(cubes))
+
+        except Exception as error:
+            raise AssertionError("Bad pcn file {}".format(filePath)) from error
+
+def write(f, numVars, cubes):
+    endl = "\n"
+
+    f.write(str(max(max(map(abs, cube)) for cube in cubes)))
+    f.write(endl)
+    f.write(str(len(cubes)))
+    f.write(endl)
+    
+    cubes = tuple(set(tuple(sorted(cube, key=abs)) for cube in cubes))
+
+    for cube in cubes:
+        f.write(' '.join(map(str, chain((len(cube),), cube))))
+        f.write(endl)
+
+    f.write(endl)
