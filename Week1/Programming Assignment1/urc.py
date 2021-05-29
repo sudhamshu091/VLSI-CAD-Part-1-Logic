fda3b1b1905490548c18a1ab3351ab12025a0385
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-05-29 22:37:50 +0530

    Update urc.py

--- Week1/Programming Assignment1/urc.py
+++ Week1/Programming Assignment1/urc.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         numVars, cubes = pcn.parse(sys.argv[1])
         cubesNot = urp.complement(cubes)
-
+ 
         pcn.write(sys.stdout, numVars, cubesNot)
     else:
         print(Usage.format(sys.argv[0]))
